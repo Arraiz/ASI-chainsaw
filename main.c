97c4c88e7e69e88b7387c0cd5321b6e293c7020b
Arraiz <mikeldiezgarcia@gmail.com>
2017-03-13 09:08:29 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Proceso Monitor %s\n",argv[1]);
+    printf("Cola de Mensajes %s\n",argv[2]);
+    return (EXIT_SUCCESS);
+}
